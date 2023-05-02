3a9dac4a27a5363ebf389a78011632ed49778148
Alexandr Zvonarev <alex@Alexandrs-MacBook-Air.local>
2023-05-02 11:39:45 +0200

    ytdghf

--- demo_flake8_bugbear.py
+++ demo_flake8_bugbear.py
@@ -1,9 +1,6 @@
 import socket
 
 
-23 < 42
-
-
 def printer_is_available(printer_host: str, printer_port: int) -> bool:
     """Check if printer is available."""
     try:
@@ -11,11 +8,11 @@ def printer_is_available(printer_host: str, printer_port: int) -> bool:
             sock.settimeout(0.5)
             sock.connect((printer_host, printer_port))
             return True
-    except:
+    except socket.error:
         return False
 
 
-def batch_check_printer(printers=[]) -> list:
+def batch_check_printer(printers) -> list:
     """Check each printer in a list for availability."""
     return [printer_is_available(*printer) for printer in printers]
 
@@ -23,8 +20,8 @@ def batch_check_printer(printers=[]) -> list:
 class DummyFloat:
     # shouldn't be calling __float__ at all when doing comparisons
     def __float__(self):
-        assert False, "__float__ should not be invoked for comparisons"
+        raise AssertionError("__float__ should not be invoked for comparisons")
 
     # same goes for subtraction
     def __sub__(self, other):
-        assert False, "__sub__ should not be invoked for comparisons"
+        raise AssertionError("__sub__ should not be invoked for comparisons")
