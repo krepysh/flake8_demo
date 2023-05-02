f33d864b4421f2373b9118fc3042772be1a3660e
vuthanh2611 <vuthanhsi246@gmail.com>
2023-05-02 15:32:03 +0700

    modify

---
+++ demo_flake8.py
@@ -0,0 +1,26 @@
+def jump():
+    pass
+
+
+def move():
+    pass
+
+
+def at_goal():
+    pass
+
+
+def front_is_clear():
+    pass
+
+
+def get_config():
+    pass
+
+
+def hurdle():
+    while not at_goal():
+        while front_is_clear():
+            move()
+        jump()
+
