079e78bf94642c86d2edc4ddcba85405a2cb9e36
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-02 07:33:01 +0200

    Files to edit

---
+++ demo_flake8.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+def jump():
+    pass
+def move():
+    pass
+
+
+def at_goal():
+    pass
+
+
+def front_is_clear():
+    pass
+
+def get_config():
+    pass
+
+def hurdle():
+    while (at_goal()== False):
+        while (front_is_clear() ==  True):
+            move()
+        jump()
---
+++ demo_flake8_bugbear.py
@@ -0,0 +1,30 @@
+import socket
+
+
+23 < 42
+
+
+def printer_is_available(printer_host: str, printer_port: int) -> bool:
+    """Check if printer is available."""
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(0.5)
+            sock.connect((printer_host, printer_port))
+            return True
+    except:
+        return False
+
+
+def batch_check_printer(printers=[]) -> list:
+    """Check each printer in a list for availability."""
+    return [printer_is_available(*printer) for printer in printers]
+
+
+class DummyFloat:
+    # shouldn't be calling __float__ at all when doing comparisons
+    def __float__(self):
+        assert False, "__float__ should not be invoked for comparisons"
+
+    # same goes for subtraction
+    def __sub__(self, other):
+        assert False, "__sub__ should not be invoked for comparisons"
